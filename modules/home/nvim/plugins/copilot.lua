5dae853f4ff37973b3997d21efbc9ba1a109843f
Cameron McNiff <112502675+CMcN-38@users.noreply.github.com>
2024-03-05 21:02:34 +0000

    Tue Mar  5 09:02:34 PM GMT 2024

--- modules/home/nvim/plugins/copilot.lua
+++ modules/home/nvim/plugins/copilot.lua
@@ -1,9 +1,7 @@
-        
-{config = function()
-                vim.keymap.set('i', '<C-Tab>', 'copilot#Accept("\\<CR>")', {
-                        expr = true,
-                        replace_keycodes = true
-                })
-                vim.g.copilot_no_tab_map = false
-        end,
-}
+config = function()
+	vim.keymap.set('i', '<C-Tab>', 'copilot#Accept("\\<CR>")', {
+		expr = true,
+		replace_keycodes = true
+	})
+	vim.g.copilot_no_tab_map = false
+end
--- modules/home/nvim/plugins/dashboard.lua
+++ modules/home/nvim/plugins/dashboard.lua
@@ -1,41 +1,39 @@
-{'nvimdev/dashboard-nvim',
-        event = 'VimEnter',
-        config = function()
-                require('dashboard').setup{
-                        theme = 'hyper',
-                        hide ={
-                                statusline,
-                                tabline,
-                                winbar
-                        },
-                        config = {
-                                week_header = {
-                                        enable = true,
-                                },
-                                shortcut = {
-                                        { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
-                                        {
-                                                icon = ' ',
-                                                icon_hl = '@variable',
-                                                desc = 'Files',
-                                                group = 'Label',
-                                                action = 'Telescope find_files',
-                                                key = 'f',
-                                        },
-                                        {
-                                                desc = ' Apps',
-                                                group = 'DiagnosticHint',
-                                                action = 'Telescope app',
-                                                key = 'a',
-                                        },
-                                        {
-                                                desc = ' dotfiles',
-                                                group = 'Number',
-                                                action = 'Files ~/.config',
-                                                key = 'd',
-                                        },
-                                },
-                        },
-                }
-        end,
-}
+event = 'VimEnter',
+config = function()
+	require('dashboard').setup{
+		theme = 'hyper',
+		hide ={
+			statusline,
+			tabline,
+			winbar
+		},
+		config = {
+			week_header = {
+				enable = true,
+			},
+			shortcut = {
+				{ desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+				{
+					icon = ' ',
+					icon_hl = '@variable',
+					desc = 'Files',
+					group = 'Label',
+					action = 'Telescope find_files',
+					key = 'f',
+				},
+				{
+					desc = ' Apps',
+					group = 'DiagnosticHint',
+					action = 'Telescope app',
+					key = 'a',
+				},
+				{
+					desc = ' dotfiles',
+					group = 'Number',
+					action = 'Files ~/.config',
+					key = 'd',
+				},
+			},
+		},
+	}
+end
