d9fec13a93eb5ab3bbd845d321bba53e87c49733
Cameron McNiff <112502675+CMcN-38@users.noreply.github.com>
2024-03-05 21:26:16 +0000

    Tue Mar  5 09:26:16 PM GMT 2024

--- modules/home/nvim/plugins/telescope.lua
+++ modules/home/nvim/plugins/telescope.lua
@@ -1,6 +1,5 @@
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<C-f>", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<C-f>", telescope.builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", telescope.builtin.live_grep, {})
 require("telescope").setup({
         pickers = {
                 find_files = {
