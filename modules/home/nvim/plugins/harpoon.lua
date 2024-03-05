14aebb0578f1d1cde16705400134ab6d13f530c5
Cameron McNiff <112502675+CMcN-38@users.noreply.github.com>
2024-03-05 22:12:06 +0000

    Tue Mar  5 10:12:06 PM GMT 2024

--- modules/home/nvim/plugins/harpoon.lua
+++ modules/home/nvim/plugins/harpoon.lua
@@ -1,12 +1,12 @@
-branch = "harpoon2"
-local harpoon = require("harpoon")
-harpoon:setup()
-vim.keymap.set("n", "<leader>ha", function()
-        harpoon:list():append()
-end)
-vim.keymap.set("n", "<leader>hl", function()
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-end)
+-- branch = "harpoon2"
+-- local harpoon = require("harpoon")
+-- harpoon:setup()
+-- vim.keymap.set("n", "<leader>ha", function()
+--         harpoon:list():append()
+-- end)
+-- vim.keymap.set("n", "<leader>hl", function()
+--         harpoon.ui:toggle_quick_menu(harpoon:list())
+-- end)
 
-vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end)
+-- vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
+-- vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end)
