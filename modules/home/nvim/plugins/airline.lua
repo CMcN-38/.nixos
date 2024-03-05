e63753d4d7480b75a104cd7bc2c8bb070aa576eb
Cameron McNiff <112502675+CMcN-38@users.noreply.github.com>
2024-03-05 20:00:13 +0000

    Tue Mar  5 08:00:13 PM GMT 2024

--- modules/home/nvim/plugins/airline.lua
+++ modules/home/nvim/plugins/airline.lua
@@ -1,9 +1,7 @@
-return {
         {"vim-airline/vim-airline"},
         {"vim-airline/vim-airline-themes",
                 config = function()
                         vim.g.airline_theme = 'base16'
                 end
         },
-}
 
