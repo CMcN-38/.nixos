939252c1e0619ed052c4b703fff95018509556df
Cameron McNiff <112502675+CMcN-38@users.noreply.github.com>
2024-03-05 19:42:12 +0000

    Test Nvim

---
+++ modules/home/nvim/options.lua
@@ -0,0 +1,21 @@
+-- COLORS
+vim.cmd('syntax on')
+vim.cmd('highlight LineNr ctermfg=grey')
+
+-- BASICS
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.autoindent = true
+vim.o.expandtab = true
+vim.o.tabstop = 8
+vim.o.shiftwidth = 8
+vim.o.smarttab = true
+vim.o.scrolloff = 25
+vim.o.clipboard = 'unnamed'
+vim.o.mouse = ""
+
+--KEYMAPS
+vim.g.mapleader = " "
+local map = vim.api.nvim_set_keymap
+map('n', '<C-t>', ':silent !tmux neww tmux-sessioniser<CR>', {noremap = true, silent = true})
+
