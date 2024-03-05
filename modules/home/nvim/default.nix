56f0778cba2d25d6ec46b7563f2b5d1fd756f43b
Cameron McNiff <112502675+CMcN-38@users.noreply.github.com>
2024-03-05 21:55:10 +0000

    Tue Mar  5 09:55:10 PM GMT 2024

--- modules/home/nvim/default.nix
+++ modules/home/nvim/default.nix
@@ -11,8 +11,8 @@
     vimdiffAlias = true;
 
     plugins = with pkgs.vimPlugins; [
-      #vim-airline
-      #vim-airline-themes
+      vim-airline
+      vim-airline-themes
 
       catppuccin-nvim
       colorizer
@@ -30,21 +30,21 @@
       dashboard-nvim
       nvim-web-devicons
 
-      harpoon
+      #harpoon
 
-      #indent-blankline-nvim
+      indent-blankline-nvim
 
-      #mason-nvim
-      #mason-lspconfig-nvim
-      #nvim-lspconfig
+      mason-nvim
+      mason-lspconfig-nvim
+      nvim-lspconfig
 
-      #neo-tree-nvim
-      #plenary-nvim
-      #nui-nvim
+      neo-tree-nvim
+      plenary-nvim
+      nui-nvim
 
-      #none-ls-nvim
+      none-ls-nvim
 
-      #tagbar
+      tagbar
 
       telescope-nvim
       telescope-ui-select-nvim
@@ -60,11 +60,11 @@
         ]);
       }
 
-      #vim-be-good
+      vim-be-good
 
-      #vim-commentary
+      vim-commentary
 
-      #which-key-nvim
+      which-key-nvim
     ];
 
     extraLuaConfig = ''
