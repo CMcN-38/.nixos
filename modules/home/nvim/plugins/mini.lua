538efc99c164e8a5f4d7dbaebfa20ea047fe0689
Cameron McNiff <112502675+CMcN-38@users.noreply.github.com>
2024-03-23 14:40:16 +0000

    Sat Mar 23 02:40:16 PM GMT 2024

--- modules/home/nvim/plugins/mini.lua
+++ modules/home/nvim/plugins/mini.lua
@@ -1,4 +1,4 @@
-local statusline = require 'mini.statesline'
+local statusline = require 'mini.statusline'
 statusline.setup { use_icons = vim.g.have_nerd_font }
 statusline.section_location = function()
         return '%2l:%-2v'
