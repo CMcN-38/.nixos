4bc7485e823cc8e785181ed374d37778d20a3e2f
Cameron McNiff <112502675+CMcN-38@users.noreply.github.com>
2024-03-23 14:50:16 +0000

    Sat Mar 23 02:50:16 PM GMT 2024

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -91,15 +91,15 @@
   #networking.firewall.allowedUDPPorts = [1373];
   # Or disable the firewall altogether.
   networking.firewall.enable = false;
-  #  networking.useDHCP = false;
-  #  networking.interfaces.wlp2s0.ipv4.addresses = [
-  #    {
-  #      address = "192.168.0.114";
-  #      prefixLength = 24;
-  #    }
-  #  ];
-  #  networking.defaultGateway = "192.168.0.1";
-  #  networking.nameservers = ["8.8.8.8"];
+  networking.useDHCP = false;
+  networking.interfaces.wlp2s0.ipv4.addresses = [
+    {
+      address = "192.168.0.114";
+      prefixLength = 24;
+    }
+  ];
+  networking.defaultGateway = "192.168.0.1";
+  networking.nameservers = ["8.8.8.8"];
 
   #┓┏┏┏┓┏┓┏
   #┗┻┛┗ ┛ ┛
