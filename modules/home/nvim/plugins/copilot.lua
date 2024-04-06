77889247fdacba7117a6f0a026364617a9d4cda7
Cameron McNiff <112502675+CMcN-38@users.noreply.github.com>
2024-04-06 22:50:19 +0100

    Sat Apr  6 10:50:19 PM BST 2024

--- modules/home/nvim/plugins/copilot.lua
+++ modules/home/nvim/plugins/copilot.lua
@@ -1,7 +1,8 @@
-vim.keymap.set('i', '<C-Tab>', 'copilot#Accept("\\<CR>")', {
+vim.keymap.set('i', 'C-space', 'copilot#Accept("\\<CR>")', {
         expr = true,
         replace_keycodes = true
 })
 vim.g.copilot_no_tab_map = true
 vim.keymap.set('n', '<leader>cpe', ':Copilot enable<CR>')
 vim.keymap.set('n', '<leader>cpd', ':Copilot disable<CR>')
+
