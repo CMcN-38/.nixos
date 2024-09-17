8ec197335e6be160621ff8eb56ba6bd01524e69c
Cameron McNiff <112502675+CMcN-38@users.noreply.github.com>
2024-09-17 09:39:07 +0100

    Tue Sep 17 09:39:07 AM BST 2024

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     #nixpkgs.url = "github:NixOS/nixpkgs/staging-next";
     home-manager = {
       url = "github:nix-community/home-manager";
