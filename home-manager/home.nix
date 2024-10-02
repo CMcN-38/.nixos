d79ee1fc32630cea4de8289b022ad087dcfafd62
Cameron McNiff <112502675+CMcN-38@users.noreply.github.com>
2024-10-02 14:09:02 +0100

    Add Hypr HM Control

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -25,39 +25,18 @@
 }: {
   imports = [
     ../modules/home/nvim/default.nix
+    # ../modules/home/hypr/default.nix
   ];
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+
   home.username = "cameron";
   home.homeDirectory = "/home/cameron";
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
+
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
+  home.enableNixpkgsReleaseCheck = false;
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
