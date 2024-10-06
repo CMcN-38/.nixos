a6263906fff52dfcc1ec528db9ce570b11abfb61
Cameron McNiff <112502675+CMcN-38@users.noreply.github.com>
2024-10-06 16:30:21 +0100

    Sun Oct  6 04:30:21 PM BST 2024

--- modules/home/kitty/stark.nix
+++ modules/home/kitty/stark.nix
@@ -4,22 +4,22 @@
   pkgs,
   ...
 }: {
-        programs.kitty.enable = true;
-        programs.kitty.shellIntegration.enableZshIntegration = true;
+  programs.kitty.enable = true;
+  programs.kitty.shellIntegration.enableZshIntegration = true;
 
-        programs.kitty.font.name = "FiraCode Nerd Font";
-        programs.kitty.font.size = "16";
+  programs.kitty.font.name = "FiraCode Nerd Font";
+  programs.kitty.font.size = 16;
 
-        programs.kitty.themeFile = "Catppuccin-Mocha" ;
+  programs.kitty.themeFile = "Catppuccin-Mocha";
 
-        programs.kitty.settings = {
-                enable_audio_bell = false;
-                confirm_os_window_close = 0;
+  programs.kitty.settings = {
+    enable_audio_bell = false;
+    confirm_os_window_close = 0;
 
-                background_opacity = 0.85;
-                background_tint = 0.0;
+    background_opacity = "0.85";
+    background_tint = "0.0";
 
-                startup_sessions launch sh -c "tmux new_session -A -D -s home";
-                };
-        
+    # startup_session = "launch.conf";
+    startup_session = "/home/cameron/.nixos/modules/home/kitty/launch.conf";
+  };
 }
