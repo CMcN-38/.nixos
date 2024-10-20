1c8574e3d4ad3a7044b492fe1dcec53a16be4e30
Cameron McNiff <112502675+CMcN-38@users.noreply.github.com>
2024-10-20 16:18:38 +0100

    Sun Oct 20 04:18:38 PM BST 2024

--- modules/home/kitty-cp/targaryen.nix
+++ modules/home/kitty-cp/targaryen.nix
@@ -12,7 +12,7 @@
 
   # programs.kitty.themeFile = "Cyberpunk-Neon";
   # programs.kitty.themeFile = "Bright_Lights";
-  programs.kitty.themeFile = "Galaxy";
+  programs.kitty.themeFile = "IR_Black";
 
   programs.kitty.settings = {
     enable_audio_bell = false;
