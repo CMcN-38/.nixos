8ab860fc0eb405203716819265675fe7afe4f9c8
Cameron McNiff <112502675+CMcN-38@users.noreply.github.com>
2025-05-11 20:42:51 +0100

    Sun May 11 08:42:51 PM BST 2025

--- modules/home/hypr/targaryen.nix
+++ modules/home/hypr/targaryen.nix
@@ -160,7 +160,7 @@
         shadow {
                 enabled = true
                 range = 4
-                power = 3
+                render_power = 3
                 color = rgba(1a1a1aee)
                 }
     }
