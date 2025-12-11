034a1ee987aa5dfa9b2eaa0d48bca687fd818516
Cameron McNiff <112502675+CMcN-38@users.noreply.github.com>
2025-12-11 17:25:48 +0000

    Thu Dec 11 05:25:48 PM GMT 2025

--- modules/home/nvim/plugins/none-ls.lua
+++ modules/home/nvim/plugins/none-ls.lua
@@ -1,9 +1,19 @@
 local null_ls = require("null-ls")
 null_ls.setup({
         sources = {
+                -- Lua
                 null_ls.builtins.formatting.stylua,
+                --Python
                 null_ls.builtins.formatting.black,
                 null_ls.builtins.formatting.isort,
+                null_ls.builtins.diagnostics.ruff,
+                -- Rust
+                null_ls.builtins.formatting.rustfmt,
+                --Nix
+                null_ls.builtins.formatting.alejandra,
+                null_ls.builtins.diagnostics.statix,
+                null_ls.builtins.formatting.deadnix,
+
         },
 })
 vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
--- modules/home/nvim/plugins/treesitter.lua
+++ modules/home/nvim/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 require('nvim-treesitter.configs').setup {
     ensure_installed = {},
 
-    auto_install = true,
+    auto_install = false,
 
     highlight = { enable = true },
 
